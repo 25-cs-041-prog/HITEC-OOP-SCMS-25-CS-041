4eb67a85a6f142dcec10aec18a9de1ed749b8841
Aimen Fatima Kayani <25-cs-041@student.hitecuni.edu.pk>
2026-06-14 04:02:18 +0500

    Update Invoice.cpp

--- src/finance/Invoice.cpp
+++ src/finance/Invoice.cpp
@@ -1,7 +1,7 @@
 /**
  * @file Invoice.cpp
  * @brief Implementation of Invoice — static counter, deep copy, destructor
- * @author [Your Name] | Roll No: [XXXX]
+ * @author Aimen Fatima | Roll No: 25-cs-041
  * @course CS-104L: Object-Oriented Programming
  * @inst HITEC University Taxila
  * @date 2025
