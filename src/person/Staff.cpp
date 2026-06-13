f43e10a632c730feca946debbf76a8f685d4113e
Aimen Fatima Kayani <25-cs-041@student.hitecuni.edu.pk>
2026-06-14 04:17:21 +0500

    Update Staff.cpp

--- src/person/Staff.cpp
+++ src/person/Staff.cpp
@@ -1,7 +1,7 @@
 /**
  * @file Staff.cpp
  * @brief Implementation of the Staff class
- * @author [Your Name] | Roll No: [XXXX]
+ * @author Aimen Fatima | Roll No: 25-cs-041
  * @course CS-104L: Object-Oriented Programming
  * @inst HITEC University Taxila
  * @date 2025
