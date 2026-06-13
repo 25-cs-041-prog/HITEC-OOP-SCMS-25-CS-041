27e2e2e4624d3f37f6bc8067a334d3cb64068c08
Aimen Fatima Kayani <25-cs-041@student.hitecuni.edu.pk>
2026-06-14 04:01:02 +0500

    Update FeeRecord.cpp

--- src/finance/FeeRecord.cpp
+++ src/finance/FeeRecord.cpp
@@ -1,7 +1,7 @@
 /**
  * @file FeeRecord.cpp
  * @brief Implementation of FeeRecord — copy semantics and operator-=
- * @author [Your Name] | Roll No: [XXXX]
+ * @author  Aimen Fatima | Roll No: 25-cs-041
  * @course CS-104L: Object-Oriented Programming
  * @inst HITEC University Taxila
  * @date 2025
