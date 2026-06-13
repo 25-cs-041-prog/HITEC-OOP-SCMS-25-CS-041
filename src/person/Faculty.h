1c8e5b41d5c89604e1964a957169bfa0fcd97b1a
Aimen Fatima Kayani <25-cs-041@student.hitecuni.edu.pk>
2026-06-14 04:14:09 +0500

    Update Faculty.h

--- src/person/Faculty.h
+++ src/person/Faculty.h
@@ -1,7 +1,7 @@
 /**
  * @file Faculty.h
  * @brief Faculty class derived from Person
- * @author [Your Name] | Roll No: [XXXX]
+ * @author Aimen Fatima | Roll No: 25-cs-041
  * @course CS-104L: Object-Oriented Programming
  * @inst HITEC University Taxila
  * @date 2025
