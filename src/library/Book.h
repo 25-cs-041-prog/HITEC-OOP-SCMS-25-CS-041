d39d89f4e3ce9723e795b9b7a47ec6fc12347a0a
Aimen Fatima Kayani <25-cs-041@student.hitecuni.edu.pk>
2026-06-14 22:15:38 +0500

    Update Book.h

--- src/library/Book.h
+++ src/library/Book.h
@@ -1,22 +1,9 @@
-/**
- * @file Book.h
- * @brief Book class derived from LibraryItem
- * @author Aimen Fatima | Roll No: 25-cs-041
- * @course CS-104L: Object-Oriented Programming
- * @inst HITEC University Taxila
- * @date 2025
- *
- * OOP Concepts: Single Inheritance, Override, Encapsulation
- */
 
 #ifndef BOOK_H
 #define BOOK_H
 
 #include "LibraryItem.h"
 
-// ─────────────────────────────────────────────────────────────
-// Book — Derived from LibraryItem
-// ─────────────────────────────────────────────────────────────
 class Book : public LibraryItem {
 private:
     string isbn;
@@ -32,18 +19,15 @@ public:
     void displayInfo()  const override;
     void returnItem()   override;
 
-    // Serialise to string for file I/O
     string toFileString() const;
 
-    // ── Getters ───────────────────────────────────────────────
     string getISBN()           const { return isbn;             }
     string getGenre()          const { return genre;            }
     int    getCopiesAvailable()const { return copiesAvailable;  }
 
-    // ── Setters ───────────────────────────────────────────────
     void setISBN(const string& i)  { isbn  = i; }
     void setGenre(const string& g) { genre = g; }
     void setCopiesAvailable(int c) { copiesAvailable = c; }
 };
 
-#endif // BOOK_H
+#endif 
